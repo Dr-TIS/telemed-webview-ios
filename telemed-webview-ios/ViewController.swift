c424388c716977dcab4c7e0c4042f3eed3a22883
Gustavo Silva <gustavo.silva@drtis.com.br>
2022-06-03 15:44:36 -0300

    initial commit

---
+++ telemed-webview-ios/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  telemed-webview-ios
+//
+//  Created by Gustavo Diogo Silva on 02/06/22.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = prefs
+        configuration.allowsInlineMediaPlayback = true
+        configuration.allowsPictureInPictureMediaPlayback = true
+        
+        if let userScriptURL = Bundle.main.url(forResource: "UserScript", withExtension: "js") {
+
+            let userScriptCode = try! String(contentsOf: userScriptURL)
+            let userScript = WKUserScript(source: userScriptCode, injectionTime: .atDocumentStart, forMainFrameOnly: false)
+
+            configuration.userContentController.addUserScript((userScript))
+        }
+        
+        
+        webView = WKWebView(
+            frame: .zero, configuration: configuration
+        )
+        
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let url = URL(string: "https://develop.telemedicina.drtis.com.br") else {
+            return
+        }
+        webView.load(URLRequest(url: url))
+        webView.scrollView.isScrollEnabled = false
+     
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+5) {
+            self.webView.evaluateJavaScript("document.body.innerHTML") { result, error in guard let html = result as? String, error == nil else {
+                return
+            }
+                print(html)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
+
+
+}
+
---
+++ telemed-webview-iosUITests/telemed_webview_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  telemed_webview_iosUITestsLaunchTests.swift
+//  telemed-webview-iosUITests
+//
+//  Created by Gustavo Diogo Silva on 02/06/22.
+//
+
+import XCTest
+
+class telemed_webview_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
